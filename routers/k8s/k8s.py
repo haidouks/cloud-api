2d07db27ffb8bf0b2d10a37f081542a7c187ffc6
Cansın Aldanmaz <canald@gmail.com>
2021-02-08 02:53:01 +0300

    Update k8s.py
    remove unused packages

--- routers/k8s/k8s.py
+++ routers/k8s/k8s.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
 from .base_models import *
 from packages.k8s import kubernetes
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from pprint import pprint
-import json
 
 router = APIRouter()  
 k8s = kubernetes()
